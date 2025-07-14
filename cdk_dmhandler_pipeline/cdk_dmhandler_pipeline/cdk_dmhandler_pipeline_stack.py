98c17077444913d5a2e6853d93c2f551167ddc40
Debarun De <debarun.de20@gmail.com>
2025-07-14 11:33:51 -0400

    test cdk

---
+++ cdk_dmhandler_pipeline/cdk_dmhandler_pipeline/cdk_dmhandler_pipeline_stack.py
@@ -0,0 +1,46 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    aws_lambda as _lambda,
+    aws_s3_notifications as s3n,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class CdkDmhandlerPipelineStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "CdkDmhandlerPipelineQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
+
+        # Import s3 Bucket
+        bucket = s3.Bucket.from_bucket_name(
+            self, "ImportedBucket",
+            bucket_name="dmhandler-build-artifacts"
+        )
+
+        lambda_fn = _lambda.Function(
+            self, "DmHandlerLambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="lambda_handler.main",
+            code=_lambda.Code.from_asset("lambda")
+        )
+
+        bucket.grant_read(lambda_fn)
+
+        #add event notification to bucket
+        notification = s3n.LambdaDestination(lambda_fn)
+        bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED,
+            notification,
+            s3.NotificationKeyFilter(prefix="release/")
+        )
